44942d64610256c382237479e8f8a9d5b6f91945
closecrowd <closecrowd@gmail.com>
2023-11-16 23:13:34 -0800

    More documentation - it's docstrings all the way down....
    Some minor fixes and cleanups that go along with the docs

--- __init__.py
+++ __init__.py
@@ -1,15 +1,14 @@
-"""
-   ApyEngine - An environment for running Python-subset scripts in a relatively
-        safe manner.
+""" ApyEngine - An environment for running Python-subset scripts.
 
-    This package incorporates "asteval" from https://github.com/newville/asteval
+Credits:
 
-    version: 1.0
-    last update: 2023-Nov-13
-    License:  MIT
-    Author:  Mark Anacker <closecrowd@pm.me>
-
-    Copyright (c) 2023 by Mark Anacker.   All Rights Reserved
+    * version: 1.0
+    * last update: 2023-Nov-13
+    * License:  MIT
+    * Author:  Mark Anacker <closecrowd@pm.me>
+    * Copyright (c) 2023 by Mark Anacker
+Note:
+    * This package incorporates "asteval" from https://github.com/newville/asteval
 
 """
 
@@ -19,8 +18,8 @@ from .astutils import (NameFinder, valid_symbol_name,
                        make_symbol_table, get_ast_names)
 from ._version import get_versions
 
-__all__ = ['AstEngine', 'Interpreter', 'NameFinder', 'valid_symbol_name',
-           'make_symbol_table', 'get_ast_names', 'ApyEngine']
+__all__ = ['ApyEngine', 'Interpreter', 'NameFinder', 'valid_symbol_name',
+           'make_symbol_table', 'get_ast_names']
 
 __version__ = get_versions()['version']
 del get_versions

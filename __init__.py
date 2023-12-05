6f5edcc89411f361d756df66a216a63094456c2b
closecrowd <closecrowd@gmail.com>
2023-12-05 13:26:07 -0800

    Updated version strings, docs

---
+++ __init__.py
@@ -0,0 +1,33 @@
+"""ApyEngine - An interpreter for running Python-subset scripts.
+
+This package contains an interpreter for a safe subset of the
+Python3 language.  It does NOT run stand-alone, but must be
+imported into a host application.
+
+The companion project "apyshell" demonstrates how to fully use and
+control this engine.
+<https://github.com/closecrowd/apyshell>
+
+Credits:
+    * version: 1.0.0
+    * last update: 2023-Nov-17
+    * License:  MIT
+    * Author:  Mark Anacker <closecrowd@pm.me>
+    * Copyright (c) 2023 by Mark Anacker
+Note:
+    * This package incorporates "asteval" from
+        https://github.com/newville/asteval
+
+"""
+
+from apyengine.apyengine import ApyEngine
+from apyengine.asteval import Interpreter
+from apyengine.astutils import (NameFinder, valid_symbol_name,
+                       make_symbol_table, get_ast_names)
+from apyengine._version import get_versions
+
+__all__ = ['ApyEngine', 'Interpreter', 'NameFinder', 'valid_symbol_name',
+           'make_symbol_table', 'get_ast_names']
+
+__version__ = get_versions()['version']
+del get_versions
